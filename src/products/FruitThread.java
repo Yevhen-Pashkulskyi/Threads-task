63fe49b93948b0584febdd61890f4f70acea184c
Yevhen-Pashkulskyi <136805571+Yevhen-Pashkulskyi@users.noreply.github.com>
2024-02-08 23:52:15 +0200

    add class

--- src/products/FruitThread.java
+++ src/products/FruitThread.java
@@ -1,4 +1,13 @@
 package products;
 
+import print.PrintProducts;
+
 public class FruitThread {
+    private String[] fruits = {"Яблуко", "Банан", "Апельсин", "Груша", "Лимон"};
+
+    public void run() {
+        PrintProducts products = new PrintProducts();
+        products.print(fruits);
+    }
 }
+
